3574c682f39d0692dcb82674589d9e5682049db9
Qiang Chen <fucus.me@gmail.com>
2024-12-25 21:03:54 -0800

    add manual edits days

--- bandit_vs_learning_to_rank/env.py
+++ bandit_vs_learning_to_rank/env.py
@@ -86,7 +86,32 @@ class RecommenderEnv:
             expected_reward += click_probability * seen_probability
             seen_probability *= (1 - self.exit_probability)
 
-        return expected_reward
+        return expected_reward    
+
+    def manual_edit_ranking(self):
+        """
+        Suppose the editor has knowledge of some titles, the selected top x% titles, half are actualy top x%, and other half are random titles
+        """
+
+        sorted_click_probabilities = sorted(enumerate(self.click_probabilities), key=lambda x: x[1], reverse=True)
+        top_percent = int(self.num_titles * 0.2)
+        top_percent_titles = sorted_click_probabilities[:top_percent]
+
+        # set seed for reproducibility and stability in different days
+        rng = np.random.default_rng(seed=1)
+        half_good_selected_titles = rng.choice(top_percent_titles, size=int(top_percent * 0.5), replace=False)
+        half_good_selected_titles_ids = [int(title_id) for title_id, _ in half_good_selected_titles]
+
+        remaining_titles = [int(title_id) for title_id, _ in sorted_click_probabilities if title_id not in half_good_selected_titles_ids]
+        half_random_selected_titles_ids = rng.choice(remaining_titles, size=int(top_percent * 0.5), replace=False)
+        
+        selected_titles_ids = np.concatenate((half_good_selected_titles_ids, half_random_selected_titles_ids))
+        rng.shuffle(selected_titles_ids)
+
+        # sort the selected titles by their click probabilities
+        other_title_ids = [int(title_id) for title_id, _ in sorted_click_probabilities if title_id not in selected_titles_ids]
+        rng.shuffle(other_title_ids)
+        return np.concatenate((selected_titles_ids, other_title_ids))
 
 class Agent:
     def __init__(self, num_titles):
@@ -101,12 +126,12 @@ class Agent:
     def collect_user_feedback(self, title_ranking, user_feedback):
         raise NotImplementedError("Subclasses must implement the collect_user_feedback method")
     
-    def day_ends(self):
+    def day_starts(self):
         """
         This method is called at the end of each day. 
         The agent can use this method to train their model if the model is designed to updated daily.
         """
-        raise NotImplementedError("Subclasses must implement the day_ends method")
+        raise NotImplementedError("Subclasses must implement the day_starts method")
 
 class BanditAgent(Agent):
     """
@@ -128,12 +153,13 @@ class BanditAgent(Agent):
             return self.make_exploitation_ranking()
 
     def understand_agent(self):
+        print("agent name: ", self.__class__.__name__)
         print("click_counts: ", self.click_counts)
         print("seen_counts: ", self.seen_counts)
         click_probabilities = self.click_counts / self.seen_counts
         print("click_probabilities: ", click_probabilities)
 
-    def day_ends(self):
+    def day_starts(self):
         pass
 
     def update_theta(self, title_ids, user_feedback):
@@ -162,6 +188,66 @@ class BanditAgent(Agent):
         title_ranking = np.argsort(expected_rewards)[::-1]
         return title_ranking
 
+
+
+class BucketSortingAgent(Agent):
+    """
+    Bucket Sorting Agent
+    """
+    def __init__(self, num_titles):
+        self.bucket_size = min(max(int(num_titles / 100), 1), 5)
+        self.num_titles = num_titles
+        self.click_counts = np.zeros(num_titles)
+        self.seen_counts = np.zeros(num_titles)
+        self.click_through_rates = np.zeros(num_titles)
+   
+    def collect_user_feedback(self, title_ranking, user_feedback):
+        self.update_parameters(title_ranking, user_feedback)
+
+    def make_ranking(self):
+        # sort by click count first, group them into 10 buckets, the clicks in each bucket are equal, means the number of clicks in each bucket is 10% of the total clicks
+        sorted_click_counts = sorted(enumerate(self.click_counts), key=lambda x: x[1], reverse=True)
+        buckets = [[] for _ in range(10)]
+        total_click_count = sum(self.click_counts)
+        accumulated_click_count = 0
+        bucket_clicks_quota = total_click_count / self.bucket_size 
+        for title_id, click_count in sorted_click_counts:
+            accumulated_click_count += click_count
+            bucket_index = min(int(accumulated_click_count / bucket_clicks_quota), self.bucket_size - 1)
+            buckets[bucket_index].append(title_id)
+        
+        # sort each bucket by click through rate
+        for bucket in buckets:
+            bucket.sort(key=lambda x: self.click_through_rates[x], reverse=True)
+        
+        # flatten the buckets
+        title_ranking = [title_id for bucket in buckets for title_id in bucket]
+        return title_ranking
+
+    def understand_agent(self):
+        print("agent name: ", self.__class__.__name__)
+        print("bucket_size: ", self.bucket_size)
+        print("click_counts: ", self.click_counts)
+        print("click_through_rates: ", self.click_through_rates)
+
+    def day_starts(self):
+        pass
+
+    def update_parameters(self, title_ids, user_feedback):
+        """
+        :param title_ids: list of title ids that were shown to the user, e.g. [0, 1, 2, ...]
+        :param user_feedback: list of feedback from user, e.g. ["CLICK", "SKIP", "NOT_SEEN", ...]
+        Update parameters based on user feedback
+        """
+        for title_id, feedback in zip(title_ids, user_feedback):
+            if feedback != "NOT_SEEN":
+                if feedback == "CLICK":
+                    self.click_counts[title_id] += 1
+                    self.seen_counts[title_id] += 1
+                elif feedback == "SKIP":
+                    self.seen_counts[title_id] += 1
+                self.click_through_rates[title_id] = self.click_counts[title_id] / self.seen_counts[title_id]
+        
 
 class PointWiseModel(nn.Module):
     def __init__(self, num_titles):
@@ -204,13 +290,17 @@ class PointWiseModelAgent(Agent):
                     self.valid_user_feedback.append(1)
                 else:
                     self.valid_user_feedback.append(0)
+            else:
+                break
         self.accumulate_user_feedback(title_ids, user_feedback)
     
     def understand_agent(self):
+        print("agent name: ", self.__class__.__name__)
         print("model parameters: ", self.model.parameters())
 
-    def day_ends(self):
-        self.train()
+    def day_starts(self):
+        if len(self.collected_training_data) > 0:
+            self.train()
 
     def accumulate_user_feedback(self, title_ids, user_feedback):
         self.collected_training_data.append((title_ids, user_feedback))
@@ -237,7 +327,7 @@ class PointWiseModelAgent(Agent):
         dataset = UserFeedbackDataset(self.valid_title_ids, self.valid_user_feedback)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
-        for epoch in range(1000):
+        for epoch in range(100):
             epoch_loss = 0.0
             data_size = 0
             for title_ids, user_feedback in dataloader:
@@ -248,7 +338,7 @@ class PointWiseModelAgent(Agent):
                 optimizer.step()
                 epoch_loss += loss.item()
                 data_size += len(title_ids)
-            if epoch % 100 == 0:
+            if epoch % 10 == 0:
                 print(f"Epoch {epoch}, data points: {data_size}, Loss per data point: {epoch_loss / data_size}")
 
         print("Training completed")
@@ -300,15 +390,19 @@ class PairWiseModelAgent(Agent):
                 clean_title_ids.append(title_id)
                 if feedback == "CLICK":
                     click_count += 1
+            else:
+                break
         if len(clean_title_ids) > 1 and click_count > 0:
             self.clean_training_data.append((clean_title_ids, clean_user_feedback))
         self.accumulate_user_feedback(title_ids, user_feedback)
     
     def understand_agent(self):
+        print("agent name: ", self.__class__.__name__)
         print("model parameters: ", self.model.parameters())
 
-    def day_ends(self):
-        self.train()
+    def day_starts(self):
+        if len(self.collected_training_data) > 0:
+            self.train()
 
     def accumulate_user_feedback(self, title_ids, user_feedback):
         self.collected_training_data.append((title_ids, user_feedback))
@@ -332,7 +426,7 @@ class PairWiseModelAgent(Agent):
         dataset = UserFeedbackDataset(self.clean_training_data)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
 
-        for epoch in range(1000):
+        for epoch in range(100):
             epoch_loss = 0.0
             data_size = 0
             for title_ids, user_feedback, n in dataloader:
@@ -344,23 +438,31 @@ class PairWiseModelAgent(Agent):
                 optimizer.step()
                 epoch_loss += loss.item()
                 data_size += len(title_ids)
-            if epoch % 100 == 0:
+            if epoch % 10 == 0:
                 print(f"Epoch {epoch}, data points: {data_size}, Loss per data point: {epoch_loss / data_size}")
 
         print("Training completed")
 
-
 if __name__ == "__main__":
-    title_size = 100
+    title_size = 1000
     user_size = 10 * title_size
     run_days = 10
+
+    # if True, the manual edit stage is included on the first few days, and the collected data those days are avaiable to the agent
+    include_manual_edit_stage = True 
+    edit_stage_days = 3
+
+    total_days = run_days + edit_stage_days if include_manual_edit_stage else run_days 
+    
     import time
     start_time = time.time()
     env = RecommenderEnv(num_titles=title_size)
     bandit = BanditAgent(num_titles=title_size)
     pointwise_agent = PointWiseModelAgent(num_titles=title_size)
     pairwise_agent = PairWiseModelAgent(num_titles=title_size)
-    agent = pairwise_agent
+    bucket_agent = BucketSortingAgent(num_titles=title_size)
+
+    agents = [bandit, bucket_agent, pointwise_agent, pairwise_agent]
 
     # sort by probability, print the title id and value
     sorted_probabilities = sorted(enumerate(env.click_probabilities), key=lambda x: x[1], reverse=True)
@@ -368,29 +470,45 @@ if __name__ == "__main__":
         print(f"title {title_id}: {round(probability, 2)}")
 
     maximum_expected_reward = env.get_maximum_expected_reward()
-    actual_reward = 0.0
-    max_reward = 0.0
-    actual_reward_list = []
-    max_reward_list = []
-    for day in range(run_days):
-        actual_reward_the_day = 0.0
-        max_reward_the_day = 0.0
-        for _ in range(user_size):
-            title_ranking = agent.make_ranking()
-            user_feedback = env.get_user_feedback(title_ranking)
-            agent.collect_user_feedback(title_ranking, user_feedback)
-            click_count = sum(1 for feedback in user_feedback if feedback == "CLICK")
-            actual_reward_the_day += click_count
-            max_reward_the_day += maximum_expected_reward
-        actual_reward += actual_reward_the_day
-        max_reward += max_reward_the_day
-        actual_reward_list.append(actual_reward)
-        max_reward_list.append(max_reward)
-        agent.day_ends()
-        print(f"Day {day}, actual_reward: {actual_reward_the_day}, max_reward: {max_reward_the_day}")
-    agent.understand_agent()
-    print("actual_reward (rounded): ", round(actual_reward, 2))
-    print("max_reward (rounded): ", round(max_reward, 2))
+    
+    for agent in agents:
+        actual_reward = 0.0
+        max_reward = 0.0
+        actual_reward_list = []
+        max_reward_list = []
+        print("agent name: ", agent.__class__.__name__)
+        for day in range(total_days):
+            actual_reward_the_day = 0.0
+            max_reward_the_day = 0.0
+            manual_edit_stage = include_manual_edit_stage and day < edit_stage_days
+            if not manual_edit_stage:
+                agent.day_starts()
+                
+            for user_idx in range(user_size):
+                if user_idx % 1000 == 0:
+                    print(f"user {user_idx} / {user_size}")
+
+                if manual_edit_stage:
+                    title_ranking = env.manual_edit_ranking()
+                else:
+                    title_ranking = agent.make_ranking()
+
+                user_feedback = env.get_user_feedback(title_ranking)
+                
+                click_count = sum(1 for feedback in user_feedback if feedback == "CLICK")
+                actual_reward_the_day += click_count
+                max_reward_the_day += maximum_expected_reward
+                agent.collect_user_feedback(title_ranking, user_feedback)
+                
+            actual_reward += actual_reward_the_day
+            max_reward += max_reward_the_day
+            actual_reward_list.append(actual_reward)
+            max_reward_list.append(max_reward)
+            
+            print(f"Day {day}, actual_reward: {actual_reward_the_day}, max_reward: {max_reward_the_day}")
+        agent.understand_agent()
+        print("actual_reward (rounded): ", round(actual_reward, 2))
+        print("max_reward (rounded): ", round(max_reward, 2))
     # import matplotlib.pyplot as plt
     # plt.plot(actual_reward_list, label="actual_reward")
     # plt.plot(max_reward_list, label="max_reward")
---
+++ run.sh
@@ -0,0 +1,1 @@
+python bandit_vs_learning_to_rank/env.py | tee "output/output_$(date +%Y%m%d_%H%M%S).txt"
